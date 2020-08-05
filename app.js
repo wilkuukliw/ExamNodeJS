d96535cef784de89f3a2c6db4da7b2d96cc2bc2c
Anna <anna.maria.wilczek@gmail.com>
2020-08-05 21:19:19 +0200

    login page

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const authRoute = require('./routes/auth.js');
 app.use(authRoute);
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log("Server is listening at port " + PORT);
